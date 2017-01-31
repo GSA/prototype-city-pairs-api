9360ae1cb6e7491f2bd646e0295e3e87d1a5e3fc
Ryan Day <ryan.day@gsa.gov>
2017-01-31 13:58:51 -0600

    Adding ID route to API

--- config/routes.js
+++ config/routes.js
@@ -37,6 +37,7 @@ module.exports.routes = {
   }
   
   , '/travel/citypairs/v0/airfares': 'CityPairsMasterController.airfares' 
+    ,'/travel/citypairs/v0/airfares/:id': 'CityPairsMasterController.airfares_with_id' 
 
   /***************************************************************************
   *                                                                          *
